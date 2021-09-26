9e032da52d8e4d46ed5d52df734a551e7bf5daf9
unknown <anju.bala1>
2021-09-26 11:10:21 +0530

    changes done

--- src/main/java/basic_learning_testng/PipeStages.java
+++ src/main/java/basic_learning_testng/PipeStages.java
@@ -4,13 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
 
 public class PipeStages {
 	WebDriver driver = InitializeDriver.getDriver();
 
 	@Test(priority = 1,enabled = false)
-	public void pipeStagesCreated() {
+	public void pipeLineNewStageSchedule() {
 		driver.findElement(By.xpath("//div/div/div/div[3]/div[2]/div[5]/div/div[2]/a/span")).click();
 		driver.findElement(By.xpath("//strong[contains(text(),'Schedule an activity')]")).click();
 		// driver.findElement(By.xpath("//div/div/div/div/div/div/table/tbody/tr/td[2]/div/div[@class='o_input_dropdown']/input/a")).click();
@@ -30,10 +31,33 @@ public class PipeStages {
 	
 	@Test(priority = 2,enabled=false)
 	public void pipeStagesNewToQualified() {
-		WebElement dragFrom = driver.findElement(By.xpath("//body/div/div/div[2]/div/div/div[3]"));
-		WebElement dropTo = driver.findElement(By.xpath("//body/div/div/div[2]/div/div[2]"));
+		WebElement dragFrom = driver.findElement(By.xpath("//body/div/div/div[2]/div/div/div[3]"));// 1st column, 3rd div
+		WebElement dropTo = driver.findElement(By.xpath("//body/div/div/div[2]/div/div[2]")); // 2nd column
 		
 		Actions dragAndDrop= new Actions(driver);
 		dragAndDrop.clickAndHold(dragFrom).moveToElement(dropTo).release().build().perform();
 	}
+	@Test(priority = 3, enabled = false)
+	public void pipeLineQualifiedtoPrepositionSchedule() {
+		driver.findElement(By.xpath("//div/div/div[2]/div[2]/div[2]/div[5]/div/div[2]/a/span")).click();
+		driver.findElement(By.xpath("//strong[contains(text(),'Schedule an activity')]")).click();
+		driver.findElement(By.xpath("//div/div/div/div/div/div/table/tbody/tr/td[2]/div/div/input")).click();
+		driver.findElement(By.xpath("//ul[@id='ui-id-1']/li[1]/a")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Mark as Done')]")).click();
+	
+	}
+	
+	@Test
+	public void pipeLineQualifiedtoPrepositionSchedule1() {
+		WebElement dragFrom = driver.findElement(By.xpath("//body/div/div/div[2]/div/div[2]/div[2]"));// 1st column, 3rd div
+		WebElement dropTo = driver.findElement(By.xpath("//body/div/div/div[2]/div/div[4]")); // 2nd column
+		
+		Actions dragAndDrop= new Actions(driver);
+		dragAndDrop.clickAndHold(dragFrom).moveToElement(dropTo).release().build().perform();
+	}
+	@AfterSuite
+	public void quitBrowser() {
+		//driver.quit();
+	}
+	
 }
